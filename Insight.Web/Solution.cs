75dfbb0a11fbb2e0df6338ddcfa3bebe720ba2a1
Johnny Oshika <johnnyoshika@gmail.com>
2016-06-27 10:25:39 -0700

    Change to .Net version 4.6.

--- Insight.Web/Solution.cs
+++ Insight.Web/Solution.cs
@@ -11,7 +11,7 @@ namespace SolutionConfiguration
     public static class Solution  
     {
         static IEnumerable<string> AssemblyNames => new[] { 
-            "Insight", "Insight.Snowball", "Insight.Jobs", "Insight.Web", "Infra", "Insight.Parsing", "Insight.Parsing.Matching", "Infra.Csv"
+            "Insight", "Insight.Snowball", "Insight.Web", "Insight.Parsing", "Insight.Parsing.Matching"
         };  
 
         public static IEnumerable<Assembly> Assemblies => AssemblyNames 
