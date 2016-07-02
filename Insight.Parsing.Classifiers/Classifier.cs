55f7a902580184b644e043b2cf7c8d9555e9d2ba
dmitrynogin <dmitryn@mail.com>
2016-07-01 20:58:06 -0700

    Add Classifier project.

---
+++ Insight.Parsing.Classifiers/Classifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Parsing.Classifiers
+{
+    public abstract class Classifier : ILabeler
+    {
+        public Classifier()
+        {
+
+        }
+
+        public IEnumerable<LabelGroup> Label(Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
